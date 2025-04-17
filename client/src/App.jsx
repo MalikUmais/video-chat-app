0df816870d4f9b5fb2882d964dc0c141cd7cf099
MalikUmais <umaisfaruq03@gmail.com>
2025-04-18 00:07:54 +0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { Home, Room } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
